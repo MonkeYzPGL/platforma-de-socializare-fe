1f016fffcd2dc136f597b42d7dd7c3c30802057f
Elecfi Sergiu <40797608+MonkeYzPGL@users.noreply.github.com>
2025-03-13 16:50:55 +0200

    First Page (#1)
    I implemented the First Page of our Application, which offers the possibility to log in, sing up or reset password to the user.

--- src/app.js
+++ src/app.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import NavigationBar from './navigation-bar'
-import Home from './home/home';
-import PersonContainer from './person/person-container'
+import FirstPage from './FirstPage/FirstPage';
+
 
 import ErrorPage from './commons/errorhandling/error-page';
-import styles from './commons/styles/project-style.css';
 
 class App extends React.Component {
 
 
     render() {
-
         return (
-            <div className={styles.back}>
+            <div>
             <Router>
                 <div>
-                    <NavigationBar />
+                    {/* <NavigationBar /> */}
                     <Switch>
 
+                        {/*Prima pagina*/}
                         <Route
                             exact
                             path='/'
-                            render={() => <Home/>}
-                        />
-
-                        <Route
-                            exact
-                            path='/person'
-                            render={() => <PersonContainer/>}
+                            render={() => <FirstPage/>}
                         />
 
                         {/*Error*/}
