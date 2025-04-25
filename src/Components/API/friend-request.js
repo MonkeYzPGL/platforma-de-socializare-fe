5ad2e295409954ee68287a37ee84046e14ab49a6
Elecfi Sergiu <40797608+MonkeYzPGL@users.noreply.github.com>
2025-04-25 18:53:34 +0300

    Added the rest API's in the FE (#18)

--- src/Components/API/friend-request.js
+++ src/Components/API/friend-request.js
@@ -18,6 +18,57 @@ function sendFriendRequest(senderId, receiverId, callback) {
   });
 }
 
+function acceptFriendRequest(senderId, receiverId, callback) {
+  const request = new Request(`${FRIEND_API}/friend-request/accept/${senderId}/${receiverId}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  RestApiClient.performRequest(request, (result, status, error) => {
+    if (status === 201) {
+      console.log("Friend request accepted!");
+    }
+    callback(result, status, error);
+  });
+}
+
+function getPendingRequests(userId, callback) {
+  const request = new Request(`${FRIEND_API}/friend-request/pending/${userId}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  RestApiClient.performRequest(request, (result, status, error) => {
+    if (status === 200) {
+      console.log("Pending requests fetched!");
+    }
+    callback(result, status, error);
+  });
+}
+
+function rejectFriendRequest(senderId, receiverId, callback) {
+  const request = new Request(`${FRIEND_API}/friend-request/reject/${senderId}/${receiverId}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  RestApiClient.performRequest(request, (result, status, error) => {
+    if (status === 200) {
+      console.log("Friend request rejected!");
+    }
+    callback(result, status, error);
+  });
+}
+
 export {
-  sendFriendRequest
+  sendFriendRequest,
+  acceptFriendRequest,
+  getPendingRequests,
+  rejectFriendRequest
 };
