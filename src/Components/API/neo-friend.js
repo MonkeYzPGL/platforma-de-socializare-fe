2572f3b37d01c68c2b40ea082a44c1646c57c462
Elecfi Sergiu <elecfis@gmail.com>
2025-04-25 18:51:50 +0300

    Added the rest API's in the FE

--- src/Components/API/neo-friend.js
+++ src/Components/API/neo-friend.js
@@ -20,7 +20,40 @@ function getFriendList(userId, callback) {
   });
 }
 
+function getSuggestedFriends(userId, callback) {
+  const request = new Request(`${HOST.friend_api}/suggested/${userId}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+    }
+  });
+
+  RestApiClient.performRequest(request, (result, status, error) => {
+    if (status === 200) {
+      console.log("Suggested friends fetched from:", request.url);
+    }
+    callback(result, status, error);
+  });
+}
+
+function deleteFriendship(userId1, userId2, callback) {
+  const request = new Request(`${HOST.friend_api}/${userId1}/${userId2}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+    }
+  });
+
+  RestApiClient.performRequest(request, (result, status, error) => {
+    if (status === 200) {
+      console.log(`Friendship between ${userId1} and ${userId2} deleted.`);
+    }
+    callback(result, status, error);
+  });
+}
+
 export { 
   getFriendList,
-  
+  getSuggestedFriends,
+  deleteFriendship
 };
