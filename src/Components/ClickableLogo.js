27f8facdc56aa0f69ff2603065bf8c9fe5d43a04
Elecfi Sergiu <elecfis@gmail.com>
2025-05-06 16:15:02 +0300

    Feed Page + Logo redirect
    I implemented the Feed Page where the logged in user can see it's friends' photos. Also, I added a message if an user has no friends and we try to give him the option to add new friends. Further more, I modified the redirect logo to go to the first page of the app if the logged in user is an admin, or to go to the feed page is the logged in user is a normal person.

--- src/Components/ClickableLogo.js
+++ src/Components/ClickableLogo.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-export default function ClickableLogo({ className = "", style = {} }) {
+export default function ClickableLogo({ className = "", style = {}, disableNavigation = false }) {
     const history = useHistory();
-  
+
+    const handleClick = () => {
+        if (!disableNavigation) {
+            history.push("/feed");
+        }
+    };
+
     return (
-      <div
-        className={className}
-        style={style}
-        onClick={() => history.push("/")}
-      />
+        <div
+            className={className}
+            style={style}
+            onClick={handleClick}
+        />
     );
-  }
-  
+}
---
+++ src/Components/UserPages/FeedPage/Feed.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import { getFriendList } from "../../API/neo-friend";
+import { getUserById, getUserPhotos } from "../../API/user-account";
+import ClickableLogo from "../../ClickableLogo";
+import SearchBar from "../../GeneralComponents/SearchBar";
+import { useHistory } from "react-router-dom";
+
+
+export default function FeedPage() {
+  const [posts, setPosts] = useState([]);
+  const history = useHistory();
+  const [hasNoFriends, setHasNoFriends] = useState(false);
+
+  useEffect(() => {
+    const loggedUser = JSON.parse(localStorage.getItem("user"));
+    if (!loggedUser) return;
+
+    getFriendList(loggedUser.id, async (friends, status) => {
+      if (status === 200 && Array.isArray(friends)) {
+        if (friends.length === 0) {
+          setHasNoFriends(true);
+          return;
+        }
+        const allPosts = [];
+
+        for (const friend of friends) {
+          await getUserById(friend.id, async (userData, statusUser) => {
+            if (statusUser === 200) {
+              await getUserPhotos(friend.id, (photoResult, statusPhoto) => {
+                if (statusPhoto === 200 && photoResult?.photos?.length > 0) {
+                  photoResult.photos.forEach((photoUrl) => {
+                    allPosts.push({
+                      photoUrl,
+                      username: userData.username,
+                      profilePicture: userData.profilePicture || "/public/poze/default-avatar.png"
+                    });
+                  });
+                }
+              });
+            }
+          });
+        }
+
+        setTimeout(() => {
+          const shuffled = allPosts.sort(() => 0.5 - Math.random());
+          setPosts(shuffled);
+        }, 1000);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="feed-page">
+      <SearchBar />
+      <ClickableLogo className="feed-logo" />
+      <div className="feed-container">
+              {hasNoFriends && (
+          <div className="no-friends-message">
+            <p>You have no friends, maybe try to add someone!</p>
+            <button onClick={() => history.push("/add-friends")} className="add-friends-button">
+              Go to Add Friends
+            </button>
+          </div>
+        )}
+        {posts.map((post, idx) => (
+          <div className="feed-card" key={idx}>
+            <img src={post.photoUrl} alt="post" className="feed-photo" />
+            <div className="feed-right">
+            <div className="feed-username"onClick={() => history.push(`/view-profile/${post.userId}`)}style={{ cursor: "pointer" }}>@{post.username}</div>
+              <div className="feed-likes">❤️ 128</div>
+              <div className="feed-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+              <input className="feed-comment-input" placeholder="Leave a comment..." disabled />
+              <button className="feed-comment-button" disabled>Post</button>
+              <div className="feed-comments">
+                <div className="comment"><b>@sample_username</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div>
+                <div className="comment"><b>@sample_username</b> Nice shot!</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
